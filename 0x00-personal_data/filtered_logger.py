f6b51a21d908e5779e1f129f8d188cdabdfdfe61
Oluwatobi <amure387@gmail.com>
2023-08-30 14:59:55 +0000

    feat: completed task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python3
 '''Module for filtered_logger'''
 import re
+from typing import List
+import logging
 
-def filter_datum(fields, redaction, message, separator):
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        '''Init Function for RedactingFormatter'''
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''Formatter function to hide sensitive data'''
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super().format(record)
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:  # nopep8
     '''Function to filter out sensitive fields'''
     pattern = fr'({"|".join(fields)})=[^{separator}]+'
     return re.sub(pattern, fr'\1={redaction}', message)
