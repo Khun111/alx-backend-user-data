46134f78c42bf816e51540441703345bb654f6dc
Oluwatobi Amure <106190886+Khun111@users.noreply.github.com>
2023-08-31 12:29:00 +0100

    Update encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 '''Module for encrypting password'''
 import bcrypt
-from typing import ByteString
 
 
-def hash_password(password: str) -> ByteString:
+def hash_password(password: str) -> bytes:
     '''Function to hash password'''
     password = password.encode('utf-8')
     return bcrypt.hashpw(password, bcrypt.gensalt())
 
 
-def is_valid(hashed_password: ByteString, password: str) -> bool:
+def is_valid(hashed_password: bytes, password: str) -> bool:
     '''Function to validate password'''
     password = password.encode('utf-8')
     return bcrypt.checkpw(password, hashed_password)
