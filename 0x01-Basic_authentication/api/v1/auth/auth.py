f9bade70b67a3fdc6127fc17bfa5a1095ee09ee8
Oluwatobi <amure387@gmail.com>
2023-09-04 15:24:21 +0000

    feat: attempted task 13

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 '''Module for Authentication'''
+import fnmatch
 from flask import request
 from typing import List, TypeVar
 
 
 class Auth:
+    '''Authentication Module'''
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         '''Function to seed paths'''
         if path is not None and excluded_paths is not None and len(excluded_paths) > 0:  # nopep8
             if path.endswith('/'):
                 path = path[:-1]
             for excluded_path in excluded_paths:
+                if fnmatch.fnmatch(path, excluded_path):
+                    return False
                 if excluded_path.endswith('/'):
                     excluded_path = excluded_path[:-1]
                 if excluded_path == path:
