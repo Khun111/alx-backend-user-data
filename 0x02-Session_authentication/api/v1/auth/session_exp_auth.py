85effcbb1dd5525f1a9b8c1d6b072716a5eda1e0
Oluwatobi <amure387@gmail.com>
2023-09-07 06:14:55 +0000

    issues with task 9

---
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+'''Module for Authentication'''
+from api.v1.auth.session_auth import SessionAuth
+
+
+class SessionExpAuth(SessionAuth):
+    '''Session Authentication Module with Expiration'''
+    session_dictionary = {}
+    def __init__(self):
+        '''Instantiation Function'''
+        from os import getenv
+        duration = getenv('SESSION_DURATION')
+        try:
+            self.session_duration = int(duration)
+        except Exception:
+            self.session_duration = 0
+    def create_session(self, user_id=None):
+        '''Create session based on parent class'''
+        try:
+            session_id = super().create_session(user_id)
+        except Exception:
+            session_id = None
+        from datetime import datetime
+        self.session_dictionary["user_id"] = user_id
+        self.session_dictionary["created_at"] = datetime.now()
+        self.user_id_by_session_id[session_id] = self.session_dictionary
+        return session_id
+    def user_id_for_session_id(self, session_id=None):
+        '''Retreives user_id with session_id'''
+        if session_id is None:
+            return None
+        if not self.user_id_by_session_id.get(session_id):
+            return None
+        if self.session_duration <= 0:
+            return self.session_dictionary.get("user_id")
+        if not hasattr(self.session_dictionary, "created_at"):
+            return None
+        from datetime import datetime, timedelta
+        expired = self.session_dictionary.get("created_at") + timedelta(seconds=self.session_duration)
+        if expired < datetime.now():
+            return None
+        return self.session_dictionary.get("user_id")
