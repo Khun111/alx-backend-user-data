ada577e4bc62aeab092d2ad2aee0369855c1e09a
Oluwatobi Amure <106190886+Khun111@users.noreply.github.com>
2023-09-07 17:35:02 +0100

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -13,14 +13,14 @@ class SessionAuth(Auth):
     def create_session(self, user_id: str = None) -> str:
         '''Generates session id for isers'''
         session_id = None if user_id is None or type(
-            user_id) != str else str(uuid4())
+            user_id) is not str else str(uuid4())
         if session_id:
             self.user_id_by_session_id[session_id] = user_id
             return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         '''Retreives user_id for session_id'''
-        if session_id is None or type(session_id) != str:
+        if session_id is None or type(session_id) is not str:
             return None
         user_id = self.user_id_by_session_id.get(session_id)
         return user_id
