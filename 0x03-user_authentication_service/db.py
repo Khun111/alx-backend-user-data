0464aa0e6051f5de0ccdd7f4c533420714cde789
Oluwatobi <amure387@gmail.com>
2023-09-11 20:21:17 +0000

    feat: completed tas 2 and 3

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,15 +1,19 @@
 """DB module
 """
+from typing import Dict
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 
 from user import Base, User
 
 class DB:
     """DB class
     """
+    valid_args = ["id", "email", "hashed_password", "session_id", "reset_token"]
 
     def __init__(self) -> None:
         """Initialize a new DB instance
@@ -33,3 +37,19 @@ class DB:
         self._session.add(user)
         self._session.commit()
         return user
+    def find_user_by(self, **kwargs: Dict) -> User:
+        '''Match user with kwargs'''
+        for arg in kwargs:
+            if arg not in self.valid_args:
+                raise InvalidRequestError
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if user is None:
+            raise NoResultFound
+        return user    
+    def update_user(self, user_id: int, **kwargs : Dict) -> None:
+        '''Update user with user_id'''
+        user = self.find_user_by(id=user_id)
+        for key, value in kwargs.items():
+            if key not in self.valid_args:
+                raise ValueError
+            user.key = value
