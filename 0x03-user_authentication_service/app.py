4a3c44357f49174f289cac8a6e2d16e6bd70b42c
Oluwatobi <amure387@gmail.com>
2023-09-13 09:03:59 +0000

    feat: implemented get_reset_token and update_password views and routes

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -63,5 +63,28 @@ def profile():
         abort(403)
 
 
+@app.route('/reset_password', methods=['POST'])
+def get_reset_password_token():
+    '''Reset Password Route'''
+    email = request.form.get('email')
+    try:
+        user = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": reset token}), 200
+    except ValueError:
+        abort(403)
+
+
+@app.route('/reset_password' methods=['PUT'])
+def update_password():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    new_password = request.form.get('new_password')
+    try:
+        user = AUTH.update_password(reset_token, new_password)
+        {"email": email, "message": "Password updated"}
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -70,3 +70,21 @@ class Auth:
         '''Updates session_id to None'''
         self._db.update_user(user_id, session_id=None)
         return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        '''Creates session id'''
+        try:
+            exist = self._db.find_user_by(email=email)
+            exist.reset_token = _generate_uuid()
+            return exist.reset_token
+        except NoResultFound:
+            raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        '''Updates user password'''
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            user.hashed_password = _hash_password(password)
+            user.reset_token = None
+        except NoResultFound:
+            raise ValueError
