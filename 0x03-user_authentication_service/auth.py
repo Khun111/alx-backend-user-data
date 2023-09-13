613d1c02b8bf2ef6b0d1074b579cf3c80a7a4693
Oluwatobi <amure387@gmail.com>
2023-09-13 09:12:48 +0000

    fix: use update_user method to handle exceptions

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -75,8 +75,9 @@ class Auth:
         '''Creates session id'''
         try:
             exist = self._db.find_user_by(email=email)
-            exist.reset_token = _generate_uuid()
-            return exist.reset_token
+            reset_token = _generate_uuid()
+            self._db.update_user(exist.id, reset_token=reset_token)
+            return reset_token
         except NoResultFound:
             raise ValueError
 
@@ -84,7 +85,7 @@ class Auth:
         '''Updates user password'''
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            user.hashed_password = _hash_password(password)
-            user.reset_token = None
+            hashed_password = _hash_password(password)
+            self._db.update_user(user.id, hashed_password=hashed_password, reset_token=None)
         except NoResultFound:
             raise ValueError
