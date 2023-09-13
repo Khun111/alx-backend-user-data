be1ee710084d41ae9aca0285ffb30f90eca43392
Oluwatobi Amure <106190886+Khun111@users.noreply.github.com>
2023-09-13 07:51:39 +0100

    Update user.py

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -7,6 +7,7 @@ Base = declarative_base()
 
 
 class User(Base):
+    '''Model for users table'''
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
